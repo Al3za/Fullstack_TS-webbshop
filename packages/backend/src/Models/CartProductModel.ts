19c1a84d21343e21d85e49f64367de2c54100f61
sandro <akecakabro@gmail.com>
2022-11-09 17:13:49 +0100

    fix backend-conflict

--- packages/backend/src/Models/CartProductModel.ts
+++ packages/backend/src/Models/CartProductModel.ts
@@ -5,13 +5,14 @@ const productsCart = new Schema({
   productName: { type: String },
   productPrice: { type: Number },
   username: { type: String },
-  adress: { type: String },
 });
 
 const productModel = model<cartProduct>("modelProduct", productsCart);
 
 export const loadAllCartProd = async (
-
+  username: any
+): Promise<cartProduct[]> => {
+  const loadAll = await productModel.find({ username }).exec();
   return loadAll;
 };
 
